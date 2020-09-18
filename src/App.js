f61b62a6dfb9d88e99af64b607ac319d551850c5
Brently Bazor <brently.bazor@gmail.com>
2020-09-18 12:38:22 -0400

    initial upload

---
+++ src/App.js
@@ -0,0 +1,224 @@
+import React, { useState } from 'react';
+import axios from 'axios'
+import { useForm } from "react-hook-form";
+
+export default function App() {
+
+	const { register, handleSubmit, watch, errors } = useForm();
+
+	const [questions, setQuestions] = useState(undefined);
+	const [quizStarted, setQuizStarted] = useState(false);
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+	const [clickedAnswers, setClickedAnswers] = useState([]);
+	const [reviewAnswers, setReviewAnswers] = useState(false);
+
+
+	const handleAnswerOptionClick = (isCorrect, answerClicked, questionNumber) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		setClickedAnswers(clickedAnswers.concat({ answer: answerClicked, questionNumber: questionNumber }))
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+
+	const formatQuestionReturnData = (inputData) => {
+
+		function shuffle(array) {
+			return array.sort(() => Math.random() - 0.5);
+		}
+
+		function formattedAnswers(answersInput, allData) {
+
+			if (allData.type !== 'boolean') {
+				answersInput = shuffle(answersInput)
+			} else {
+				return [
+					{ answerText: 'True', isCorrect: 'True' === allData.correct_answer ? true : false },
+					{ answerText: 'False', isCorrect: 'False' === allData.correct_answer ? true : false }
+				]
+			}
+
+			return answersInput.map((answer) => {
+				return { answerText: htmlEntities(answer), isCorrect: answer === allData.correct_answer ? true : false }
+			})
+		}
+
+		let questionData = inputData.map((data) => {
+			let answers = [data.correct_answer, ...data.incorrect_answers]
+
+			answers = formattedAnswers(answers, data)
+
+			return { ...data, question: htmlEntities(data.question), answerOptions: answers }
+		})
+
+		return questionData
+	}
+
+	const handelGetQuestions = async data => {
+		const { numberOfQuestions, questionType, questionDifficulty } = data;
+		setQuizStarted(true)
+
+		// setQuestions(formatQuestionReturnData(questions2))
+		// return
+
+		let questionsAPIUrl = `https://opentdb.com/api.php?`
+
+		if (numberOfQuestions) {
+			questionsAPIUrl = questionsAPIUrl + `&amount=${numberOfQuestions}`
+		}
+
+		if (questionType) {
+			questionsAPIUrl = questionsAPIUrl + `&type=${questionType}`
+		}
+
+		if(questionDifficulty) { 
+			questionsAPIUrl = questionsAPIUrl + `&difficulty=${questionDifficulty}`
+		}
+
+		console.log(questionsAPIUrl);
+
+		await axios.get(questionsAPIUrl)
+			.then(res => {
+				let questionData = formatQuestionReturnData(res.data.results)
+				setQuestions(questionData)
+
+			})
+			.catch(err => {
+				console.log(err);
+			})
+
+	};
+
+	function htmlEntities(str) {
+		let newString = String(str)
+			.replace(/&amp;/g, `&`)
+			.replace(/&lt;/g, '<')
+			.replace(/&gt;/g, '>')
+			.replace(/&quot;/g, `'`)
+			.replace(/&#039;/g, `'`)
+			.replace(/&deg;/g, `\u00B0`)
+			.replace(/&prime;/g, `'`)
+
+			
+		return newString;
+
+	}
+
+	const finishQuiz = () => {
+		setQuizStarted(false);
+		setScore(0);
+		setQuestions(undefined);
+		setShowScore(false);
+		setClickedAnswers([])
+		setReviewAnswers(false)
+		setCurrentQuestion(0)
+	}
+	return (
+		<div className='app'>
+			{(questions === undefined || !quizStarted) ? (
+				<div className='menu-section'>
+					Cool Quiz
+					{/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+					<form onSubmit={handleSubmit(handelGetQuestions)}>
+						{/* register your input into the hook by invoking the "register" function */}
+						<label>Number of Questions</label>
+						<input name={'numberOfQuestions'} defaultValue={10} ref={register} />
+
+						<label>Type of Questions</label>
+						<select name="questionType" id="questionType" ref={register}>
+							<option value="">Any Type</option>
+							<option value="multiple">Multiple Choice</option>
+							<option value="boolean">True/False</option>
+						</select>
+
+						<label>Type of Category</label>
+						<select name="trivia_category" className="form-control" ref={register}>
+							<option value="any">Any Category</option>
+							<option value="9">General Knowledge</option>
+							<option value="10">Entertainment: Books</option>
+							<option value="11">Entertainment: Film</option>
+							<option value="12">Entertainment: Music</option>
+							<option value="13">Entertainment: Musicals &amp; Theatres</option>
+							<option value="14">Entertainment: Television</option>
+							<option value="15">Entertainment: Video Games</option>
+							<option value="16">Entertainment: Board Games</option>
+							<option value="17">Science &amp; Nature</option>
+							<option value="18">Science: Computers</option>
+							<option value="19">Science: Mathematics</option>
+							<option value="20">Mythology</option>
+							<option value="21">Sports</option>
+							<option value="22">Geography</option>
+							<option value="23">History</option>
+							<option value="24">Politics</option>
+							<option value="25">Art</option>
+							<option value="26">Celebrities</option>
+							<option value="27">Animals</option>
+							<option value="28">Vehicles</option>
+							<option value="29">Entertainment: Comics</option>
+							<option value="30">Science: Gadgets</option>
+							<option value="31">Entertainment: Japanese Anime &amp; Manga</option>
+							<option value="32">Entertainment: Cartoon &amp; Animations</option>
+						</select>
+
+						<label>Question Difficulty</label>
+						<select name="questionDifficulty" className="form-control" ref={register}>
+							<option value="">Any Difficulty</option>
+							<option value="easy">Easy</option>
+							<option value="medium">Medium</option>
+							<option value="hard">Hard</option>
+						</select>
+						{/* include validation with required or other standard HTML validation rules */}
+						{/* <input name="exampleRequired" ref={register({ required: true })} /> */}
+						{/* errors will return when field validation fails  */}
+						{/* {errors.exampleRequired && <span>This field is required</span>} */}
+
+						<input className={'centerButton'} type="submit" value="Start" />
+					</form>
+				</div>
+			) : showScore ? (
+				<div className='score-section'>
+					<p>You scored {score} out of {questions.length}</p>
+					<button className={'centerButton'} onClick={() => setReviewAnswers(!reviewAnswers)}>{reviewAnswers === false ? 'Review' : 'Hide'} Answers</button>
+					<button className={'centerButton'} onClick={finishQuiz}>Back To Menu</button>
+					{reviewAnswers &&
+						clickedAnswers.map((ans) => (
+							<div>
+								<h5>{questions[ans.questionNumber].question}</h5>
+								<p>You answered: {ans.answer}</p>
+								<p>Correct answer: {questions[ans.questionNumber].correct_answer}</p>
+							</div>
+						))
+					}
+				</div>
+			) : (
+						<>
+							<div className='question-section'>
+								<div className='question-count'>
+									<span>Question {currentQuestion + 1}</span>/{questions.length}
+								</div>
+								<div className='question-text'>{questions[currentQuestion].question}</div>
+								<div>{questions[currentQuestion].correct_answer}</div>
+								<div>{questions[currentQuestion].difficulty}</div>
+							</div>
+							<div className='answer-section'>
+								{questions[currentQuestion].answerOptions.map((answerOption) => (
+									<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect, answerOption.answerText, currentQuestion)}>{answerOption.answerText}</button>
+								))}
+							</div>
+						</>
+					)
+
+			}
+
+		</div>
+	);
+}
---
+++ src/__mocks__/questions.js
@@ -0,0 +1,74 @@
+const questionsOld = [
+    {
+        questionText: 'What is the capital of France?',
+        answerOptions: [
+            { answerText: 'New York', isCorrect: false },
+            { answerText: 'London', isCorrect: false },
+            { answerText: 'Paris', isCorrect: true },
+            { answerText: 'Dublin', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Who is CEO of Tesla?',
+        answerOptions: [
+            { answerText: 'Jeff Bezos', isCorrect: false },
+            { answerText: 'Elon Musk', isCorrect: true },
+            { answerText: 'Bill Gates', isCorrect: false },
+            { answerText: 'Tony Stark', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'The iPhone was created by which company?',
+        answerOptions: [
+            { answerText: 'Apple', isCorrect: true },
+            { answerText: 'Intel', isCorrect: false },
+            { answerText: 'Amazon', isCorrect: false },
+            { answerText: 'Microsoft', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'How many Harry Potter books are there?',
+        answerOptions: [
+            { answerText: '1', isCorrect: false },
+            { answerText: '4', isCorrect: false },
+            { answerText: '6', isCorrect: false },
+            { answerText: '7', isCorrect: true },
+        ],
+    },
+];
+
+let questions2 = [
+    {
+        "category": "Entertainment: Video Games", "type": "multiple", "difficulty": "easy", "question": "Who created the digital distribution platform Steam?", "correct_answer": "Valve", "incorrect_answers": ["Pixeltail Games", "Ubisoft", "Electronic Arts"]
+    },
+    {
+        "category": "Entertainment: Books", "type": "multiple", "difficulty": "hard", "question": "What is Hermione Granger&#039;s middle name?", "correct_answer": "Jean", "incorrect_answers": ["Jane", "Emma", "Jo"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "multiple", "difficulty": "medium", "question": "Where was the Sniper character in Team Fortress 2 born?", "correct_answer": "New Zealand", "incorrect_answers": ["South Africa", "Antarctica", "Australia"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy", "question": "Donkey Kong was originally set to be a Popeye arcade game.", "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "multiple", "difficulty": "hard", "question": "According to Toby Fox, what was the method to creating the initial tune for Megalovania?", "correct_answer": "Singing into a Microphone", "incorrect_answers": ["Playing a Piano", "Using a Composer Software", "Listened to birds at the park"]
+    },
+    {
+        "category": "History", "type": "multiple", "difficulty": "medium", "question": "What was the bloodiest single-day battle during the American Civil War?", "correct_answer": "The Battle of Antietam", "incorrect_answers": ["The Siege of Vicksburg", "The Battle of Gettysburg", "The Battles of Chancellorsville"]
+    },
+    {
+        "category": "Geography", "type": "multiple", "difficulty": "medium", "question": "What is the highest mountain in the world?", "correct_answer": "Mt. Everest", "incorrect_answers": ["Mount Godwin Austen", "Kangchenjunga", "Annapurna"]
+    },
+    {
+        "category": "History", "type": "multiple", "difficulty": "hard", "question": "What was the aim of the &quot;Umbrella Revolution&quot; in Hong Kong in 2014?", "correct_answer": "Genuine universal suffrage", "incorrect_answers": ["Gaining Independence", "Go back under British Rule", "Lower taxes"]
+    },
+    {
+        "category": "Entertainment: Board Games", "type": "multiple", "difficulty": "easy", "question": "In which year was the pen and paper RPG &quot;Deadlands&quot; released?", "correct_answer": "1996", "incorrect_answers": ["2003", "1999", "1993"]
+    },
+    {
+        "category": "Entertainment: Music", "type": "multiple", "difficulty": "easy", "question": "&quot;Hallelujah&quot; is a song written by which Canadian recording artist?", "correct_answer": "Leonard Cohen", "incorrect_answers": ["Kory Lefkowits", "Ryan Letourneau ", "Justin Bieber "]
+    },
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "easy", "question": "Water always boils at 100&deg;C, 212&deg;F, 373.15K, no matter where you are.", "correct_answer": "False", "incorrect_answers": ["True"]
+    }
+]
